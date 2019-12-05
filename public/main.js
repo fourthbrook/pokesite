ba06b2eec1c6bc05a70c040881983c8edabe7118
Jacob Brook <brookjacob@gmail.com>
2019-12-04 19:11:41 -0700

    reordered script calls

--- public/main.js
+++ public/main.js
@@ -2,7 +2,3 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.collapsible');
     var instances = M.Collapsible.init(elems);
 });
-
-var instance = M.Collapsible.getInstance(elem);
-console.log("hello")
-instance.open(3);
